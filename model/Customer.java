ff52bb6c8cc4b495744f2c0082331564fc49e314
malarms <malar.khurinji.btech2023@sitpune.edu.in>
2025-04-21 00:21:57 +0530

    Create Customer.java

---
+++ model/Customer.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String joinDate;
+
+    // Getters and Setters
+    // Constructor
+    // toString()
+}
