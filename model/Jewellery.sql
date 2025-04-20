626e782edb7351c9316524347cee569575a9b34b
malarms <malar.khurinji.btech2023@sitpune.edu.in>
2025-04-21 00:21:28 +0530

    Update and rename jewellery_store_schema.sql to model/Jewellery.sql

---
+++ model/Jewellery.sql
@@ -0,0 +1,16 @@
+package model;
+
+public class Jewellery {
+    private int id;
+    private String name;
+    private String type;
+    private String material;
+    private double weight;
+    private double price;
+    private int stockQuantity;
+    private String dateAdded;
+
+    // Getters and Setters
+    // Constructor
+    // toString()
+}
