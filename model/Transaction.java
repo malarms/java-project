ed4ccb3ab7b71dc8d5d43fa592b0cab84e6ec96f
malarms <malar.khurinji.btech2023@sitpune.edu.in>
2025-04-21 00:22:34 +0530

    Create Transaction.java

---
+++ model/Transaction.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Transaction {
+    private int id;
+    private int customerId;
+    private int jewelleryId;
+    private int quantity;
+    private double totalPrice;
+    private String transactionDate;
+    private String paymentMethod;
+
+    // Getters and Setters
+    // Constructor
+    // toString()
+}
